4d6a7e65195367d9dba60121f810a20de9a0c941
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-02-11 20:26:30 +0300

    Rendering of UncontrolledAccordion components

---
+++ src/components/UncontrolledAccordion/UncontrolledAccordion.tsx
@@ -0,0 +1,49 @@
+import React, {useState} from "react";
+
+type AccordionPropsType = {
+    titleValue: string
+    collapsed?: boolean
+}
+
+function UncontrolledAccordion(props: AccordionPropsType) {
+    console.log("Accordion rendering")
+
+    let [collapsed, setCollapsed] = useState(true)
+
+    const onClickHandler = () => {
+        setCollapsed(!collapsed)
+    }
+
+    return (
+        <div>
+            <AccordionTitle title={props.titleValue} onClick={onClickHandler}/>
+            {!collapsed && <AccordionBody title={props.titleValue}/>}
+        </div>
+    )
+}
+
+
+type AccordionTitlePropsType = {
+    title: string
+    onClick: () => void
+}
+
+function AccordionTitle(props: AccordionTitlePropsType) {
+    return <h3 onClick={props.onClick}>-- {props.title} --</h3>
+}
+
+type AccordionBodyPropsType = {
+    title: string
+}
+
+function AccordionBody(props: AccordionBodyPropsType) {
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+}
+
+export default UncontrolledAccordion
