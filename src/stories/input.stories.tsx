75f66592b4fd2a1de637893479cc0a328d0ba238
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-02-13 21:06:52 +0300

    story for UncontrolledInput components using links

---
+++ src/stories/input.stories.tsx
@@ -0,0 +1,34 @@
+import React, {ChangeEvent, useRef, useState} from "react";
+
+export default {
+    title: 'Input',
+};
+
+
+export const UncontrolledInput = () => <input/>
+export const TrackValueOfUncontrolledInput = () => {
+
+    const [value, setValue] = useState('')
+
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setValue(e.currentTarget.value)
+    }
+    return <><input onChange={onChange}/> - {value}</>
+}
+
+export const GetValueOfcontrolledInputByButtonPress = () => {
+    const [value, setValue] = useState('')
+
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const save = () => {
+        const el = inputRef.current as HTMLInputElement;
+        setValue(el.value);
+    }
+
+    return <><input ref={inputRef}/>
+        <button onClick={save}>save</button>
+        - actual value : {value} </>
+}
+
+export const ControlledInputWithFixedValue = () => <input value={'it-incubator.by'}/>
