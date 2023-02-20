e577f7622550c3d310fd5398a9abcdf0bbd36ce3
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-02-20 03:54:55 +0300

    Creating a react memo and wrapping components in a container

---
+++ src/stories/ReactMemo.stories.tsx
@@ -0,0 +1,39 @@
+import React, {useState} from "react";
+
+export default {
+    title:"React.memo demo"
+}
+
+
+const NewMessagesCounter = (props:{count:number}) => {
+  return <div>{props.count}</div>
+}
+
+
+
+const UsersSecret = (props:{users: Array<string>}) => {
+    return (
+        <div>{
+            props.users.map((u,i)=> <div key={i}>{u}</div>)
+        }
+        </div>
+    );
+}
+
+const Users = React.memo(UsersSecret);
+
+export const Example1 = () => {
+    const [counter,setCounter] = useState(10);
+    const [users,setUsers] = useState(["Vova","Dima","Victor"])
+
+        const addUser = () => {
+        const newUsers = [...users,'Sveta' + new Date().getTime() ]
+            setUsers(newUsers);
+        }
+    return <>
+        <button onClick={()=> {setCounter(counter+1)}}>+</button>
+        <button onClick={addUser}>add user</button>
+            <NewMessagesCounter count={counter}/>
+            <Users users={users}/>
+    </>
+}
